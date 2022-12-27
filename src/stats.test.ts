81cec1c19c5e4bdc0a98c19cc6dd0f3fb4b2c895
Javi <javier@typeform.com>
2022-12-27 21:51:47 +0100

    feat: my testing library

---
+++ src/stats.test.ts
@@ -0,0 +1,27 @@
+import {average, sum} from './stats';
+import * as statsAsync from './statsAsync';
+import {expect, test} from "./testLib";
+
+test('calculates the sum of all elements of the array', () =>{
+    let result = sum([1,2,3])
+    let expected = 6
+    expect(result).toBe(expected)
+})
+
+test('calculates the average of all elements of the array', async () =>{
+    const result = await statsAsync.average([1,2,3])
+    const expected = 2
+    expect(result).toBe(expected)
+})
+
+test('calculates the sum of all elements of the array (async)', async() =>{
+    let result = await statsAsync.sum([1,2,3])
+    let expected = 6
+    expect(result).toBe(expected)
+})
+
+test('calculates the average of all elements of the array (async)', () =>{
+    const result = average([1,2,3])
+    const expected = 2
+    expect(result).toBe(expected)
+})
---
+++ src/testLib.ts
@@ -0,0 +1,21 @@
+export async function test(description:string, callback:()=>void) {
+    try {
+        await callback()
+        console.log(`✅ ${description}`)
+    }
+    catch (error) {
+        console.log(`❌ ${description}`)
+        console.log(error)
+
+    }
+
+}
+export function expect<T>(result: T) {
+    return {
+        toBe: function (expected: T) {
+            if (result !== expected) {
+                throw new Error(`❌ ${result} is not equal to ${expected}`)
+            }
+        }
+    }
+}
